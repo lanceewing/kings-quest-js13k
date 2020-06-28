932b1415b844c5d1e44e406a89f72755feecaa6c
Lance Ewing <lance.ewing@gmail.com>
2020-06-29 00:00:53 +0100

    Removed unused methods for Utils.

--- Util.js
+++ Util.js
@@ -1,22 +1,6 @@
 class Util {
 
     /**
-     * Utility function for obtaining a 2D canvas from a newly created canvas of the 
-     * given width and height.
-     * 
-     * @param {number} w The width of the canvas.
-     * @param {number} h The height of the canvas.
-     * 
-     * @returns {CanvasRenderingContext2D} The created 2D canvas context.
-     */
-    static create2dContext(w, h) {
-        let canvas = document.createElement('canvas');
-        canvas.width = w;
-        canvas.height = h || w;
-        return (canvas.getContext('2d'));
-    }
-
-    /**
      * Linear congruential generator algorithm with a fixed seed. Gives the appearance
      * of being random but always generates the numbers in the same sequence. This has
      * been done deliberately.
