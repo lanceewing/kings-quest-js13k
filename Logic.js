74e24c6a29df1a7b9e0f2c6ba71ed7901966eec1
Lance Ewing <lance.ewing@gmail.com>
2020-07-19 23:30:06 +0100

    Added untested code for Walk to without a thing.

--- Logic.js
+++ Logic.js
@@ -3,41 +3,47 @@
  */
 class Logic {
 
-    /**
-     * Constructor for Logic.
-     * 
-     * @param {Game} game The Game.
-     */
-    constructor(game) {
-        this.game = game;
-        this.userInput = game.userInput;
-    }
-
-    /**
-     * Processes a command from the user input.
-     * 
-     * @param {string} verb The verb part of the command to process.
-     * @param {string} cmd The full command to process.
-     * @param {string} thing The thing or noun part of the command to process.
-     * @param {MouseEvent} e The mouse event associated with the command to process.
-     */
-    process(verb, cmd, thing, e) {
-      let newCommand = cmd;
-      let thingId = thing.replace(' ','_');
-  
-      // If we don't yet have a thing to do something to, then ignore.
-      if (!thing) return verb;
-
-      switch (verb) {
-
-  
-        default:
-          this.game.ego.say("Nothing happened.", 220);
-          break;
-      }
-      
-      return newCommand;
-    }
-
+  /**
+   * Constructor for Logic.
+   * 
+   * @param {Game} game The Game.
+   */
+  constructor(game) {
+    this.game = game;
+    this.userInput = game.userInput;
   }
-  
+
+  /**
+   * Processes a command from the user input.
+   * 
+   * @param {string} verb The verb part of the command to process.
+   * @param {string} cmd The full command to process.
+   * @param {string} thing The thing or noun part of the command to process.
+   * @param {MouseEvent} e The mouse event associated with the command to process.
+   */
+  process(verb, cmd, thing, e) {
+    let newCommand = cmd;
+    let thingId = thing.replace(' ', '_');
+
+    // If we don't yet have a thing to do something to, then ignore.
+    if (!thing) return verb;
+
+    switch (verb) {
+
+      case 'Walk to':
+        // TODO: Fix this
+        this.game.ego.stop(true);
+        let z = ((e.pageY / this.game.scaleY) - 27) * 2;
+        this.game.ego.moveTo($.screenLeft + (e.pageX / $.scaleX), ((e.pageY / $.scaleY) - 27) * 2);
+
+        break;
+
+      default:
+        this.game.ego.say("Nothing happened.", 220);
+        break;
+    }
+
+    return newCommand;
+  }
+
+}
